fca1c81bae0ae9eb4d6457c92f8a87b0f94e57de
Leonel Nievas Boso <leo_93_t1@hotmail.com>
2023-10-17 22:22:52 -0300

    Implementacion en proceso

--- src/lab1proyectotransversal/accesoADatos/MateriaData.java
+++ src/lab1proyectotransversal/accesoADatos/MateriaData.java
@@ -12,12 +12,52 @@ public class MateriaData {
 
     private Connection connection;
 
-    public MateriaData(Connection connection) {
-        this.connection = connection;
+    public MateriaData() {
+        this.connection = Conexion.getInstance();
     }
 
-    public void guardarMateria(Materia materia) {
+    public boolean guardarMateria(Materia materia) {
+        boolean result = true;
 
+        try {
+            // Preparar la estructura de la consulta
+            String sql = "INSERT INTO materia(nombre, año, estado, idMateria) VALUES (?, ?, ?, ?)";
+            if (materia.getIdMateria() == -1) {
+                sql = "INSERT INTO materia(nombre, año, estado) VALUES (?, ?, ?)";
+            }
+
+            // Prepared Statement
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, materia.getNombre());
+            ps.setInt(2, materia.getAnio());
+            ps.setBoolean(3, materia.isEstado());
+
+            // Si idMateria == -1 entonces se asigna automaticamente
+            if (materia.getIdMateria() != -1) {
+                ps.setInt(4, materia.getIdMateria());
+            }
+
+            // Ejecutar querry
+            int filas = ps.executeUpdate();
+            if (filas > 0) {
+                System.out.println("Alumno agregado");
+            }
+
+            // Cerrar el preparedStatement
+            ps.close();
+
+        } catch (SQLException e) {
+            result = false;
+            int errorCode = e.getErrorCode();
+            if (errorCode != 1062) { // Ignorar materias repetidas
+                System.out.println("[Error " + errorCode + "] " + e.getMessage());
+                e.printStackTrace();
+            } else {
+                System.out.println("[Materia repetida] " + e.getMessage());
+            }
+        }
+
+        return result;
     }
 
     public Materia buscarMateria(int idMateria) {
@@ -30,12 +70,14 @@ public class MateriaData {
         return null;
     }
 
-    void modificarMateria(Materia materia) {
+    public boolean modificarMateria(Materia materia) {
         // TODO: implementar este metodo
+        return false;
     }
 
-    void eliminarMateria(int idMateria) {
+    public boolean eliminarMateria(int idMateria) {
         // TODO: implementar este metodo
+        return false;
     }
 
 }
